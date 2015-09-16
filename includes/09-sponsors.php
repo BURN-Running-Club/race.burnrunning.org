526df128143fc0f3eacdbca25d47292950ee322b
chentingdong <chentingdong@gmail.com>
2015-09-16 01:27:16 -0700

    fix sponsor logos

--- includes/09-sponsors.php
+++ includes/09-sponsors.php
@@ -4,19 +4,20 @@
 		<div class="row">
 			<img class="col-xs-4 col-sm-3 col-md-2" src="images/sponsors/sportsbasement_logo.png" alt="Sports Basement" title="Sports Basement"/>
 			<img class="col-xs-4 col-sm-3 col-md-2" src="images/sponsors/bayclub_logo.png" alt="Sports Basement" title="Sports Basement"/>
-			<img class="col-xs-4 col-sm-3 col-md-2" src="images/sponsors/traderjoes_logo.png" alt="Trader Joe's" title="Trader Joe's" />
-			<img class="col-xs-4 col-sm-3 col-md-2" src="images/sponsors/wholefoods_logo.png" alt="Whole Foods" title="Whole Foods"/>
-			<img class="col-xs-4 col-sm-3 col-md-2" src="images/sponsors/hammer_logo.png" alt="Hammer" title="Hammer"/>
 			<img class="col-xs-4 col-sm-3 col-md-2" src="images/sponsors/shanghai_logo.png" alt="上海家常菜(Shanghai Delight)" title="上海家常菜(Shanghai Delight)"/>
+			<img class="col-xs-4 col-sm-3 col-md-2" src="images/sponsors/traderjoes_logo.png" alt="Trader Joe's" title="Trader Joe's" />
+			<img class="col-xs-4 col-sm-3 col-md-2" src="images/sponsors/tsinghua_logo.png" alt="清华(Tsing Hua)" title="清华(Tsing Hua University)"/>
+			<img class="col-xs-4 col-sm-3 col-md-2" src="images/sponsors/hammer_logo.png" alt="Hammer" title="Hammer"/>
 			<img class="col-xs-4 col-sm-3 col-md-2" src="images/sponsors/unionexpresstravel_logo.png" alt="Union Express Travel" title="Union Express Travel"/>
 			<img class="col-xs-4 col-sm-3 col-md-2" src="images/sponsors/fitgeeksports_logo.png" alt="Fit Geek Sports" title="Fit Geek Sports" />
-			<img class="col-xs-4 col-sm-3 col-md-2" src="images/sponsors/nulifedermspa_logo.png" alt="Nuli" title="Nuli"/>
-			<img class="col-xs-4 col-sm-3 col-md-2" src="images/sponsors/tsinghua_logo.png" alt="清华(Tsing Hua)" title="清华(Tsing Hua University)"/>
 			<img class="col-xs-4 col-sm-3 col-md-2" src="images/sponsors/xuchangfu_logo.png" alt="Xu Chang Fu" title="Xu Shi Fu"/>
-			<img class="col-xs-4 col-sm-3 col-md-2" src="images/sponsors/wfg_logo.png" alt="WFG" title="WFG"/>
+			<img class="col-xs-4 col-sm-3 col-md-2" src="images/sponsors/nulifedermspa_logo.png" alt="Nuli" title="Nuli"/>
+			<img class="col-xs-4 col-sm-3 col-md-2" src="images/sponsors/zombie_runner_logo.png" alt="Zombie Runner" title="Zombie Runner"/>
+			<img class="col-xs-4 col-sm-3 col-md-2" src="images/sponsors/wholefoods_logo.png" alt="Whole Foods" title="Whole Foods"/>
 			<img class="col-xs-4 col-sm-3 col-md-2" src="images/sponsors/ranch99_logo.png" alt="Ranch 99" title="Ranch 99"/>
-			<img class="col-xs-4 col-sm-3 col-md-2" src="images/sponsors/eastbayregionalpark_logo.png" alt="Ranch 99" title="Ranch 99"/>
-			<img class="col-xs-4 col-sm-3 col-md-2" src="images/sponsors/zombie_runner_logo.png" alt="Ranch 99" title="Ranch 99"/>
+			<img class="col-xs-4 col-sm-3 col-md-2" src="images/sponsors/wfg_logo.png" alt="WFG" title="WFG"/>
+			<img class="col-xs-4 col-sm-3 col-md-2" src="images/sponsors/devryuniv_logo.png" alt="Devry University" title="Devry University"/>
+			<img class="col-xs-4 col-sm-3 col-md-2" src="images/sponsors/eastbayregionalpark_logo.png" alt="East Bay Regional Park" title="East Bay Regional Park"/>
 		</div>
 	</div>
 </section>
