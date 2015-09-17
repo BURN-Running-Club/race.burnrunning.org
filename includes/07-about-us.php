ee6b62f5d3be772ef87550896d678b089a42384d
chentingdong <chentingdong@gmail.com>
2015-09-16 23:29:03 -0700

    fix link

--- includes/07-about-us.php
+++ includes/07-about-us.php
@@ -4,7 +4,8 @@
 	</figure>
 	<div class="content fg-text">
 		<div class="container">
-			<p><strong>BURN Running Club</strong> is a 501(c)3 not-for-profit organization. We promotes a healthy, motivated, socially responsible lifestyle. Our aspiration is to change lives around us. </p>
+			<p><strong><a href="http://burnrunning.org">BURN Running Club</a></strong> is a 501(c)3 not-for-profit
+				organization. We promotes a healthy, motivated, socially responsible lifestyle. Our aspiration is to change lives around us. </p>
 
 			<p>Our mission: BURN creates a safe and encouraging environment to support and guide runners of different levels to achieve their own health and performance goals. BURN aim to influence the local community at large to adopt an active lifestyle through events and education. </p>
 
