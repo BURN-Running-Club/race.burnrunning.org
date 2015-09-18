eeffe58d07543439f657b76019fe4a5c39b725bb
chentingdong <chentingdong@gmail.com>
2015-09-18 01:40:15 -0700

    add google analytics

--- includes/10-footer.php
+++ includes/10-footer.php
@@ -2,3 +2,13 @@
 	<a class="btn hidden-xs pull-right" target="_blank" href="http://burn2015.racewire.com/">REGISTER TODAY</a>
 	<a class="btn visible-xs" target="_blank" href="http://burn2015.racewire.com/">REGISTER TODAY</a>
 </section>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-67812639-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
