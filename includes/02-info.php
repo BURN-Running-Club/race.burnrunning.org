93c3928670a7f66e088eeeadb24f178ef66ce2ae
chentingdong <chentingdong@gmail.com>
2015-09-18 01:22:36 -0700

    add more text to info and course sections

--- includes/02-info.php
+++ includes/02-info.php
@@ -1,19 +1,17 @@
 <section id="info" class="text-center container-fluid">
 	<div class="container">
-		<div class="">
-			<h2>BURN 2015</h2>
-			<h1>5K &amp; HALF MARATHON</h1>
-			<p><strong>On Saturday, October 10, 2015</strong>, experience a festive walk or run through the
-				beautiful Coyote Hills Regional Park. Both 5K and Half Marathon courses are
-				supported by our dedicated BURN volunteers. You will never run out of
-				hydration, snacks and cheers! Half marathon finishers will receive an inaugural
-				BURN 5K &amp; Half Marathon commemorative run shirt. Bring your family members, friends, co-workers and community groups to this non-competitive, high energy, fun run and walk. <br>
-				<strong>Run with us, BURN bright, you will be our star!</strong>
-			</p>
+		<h2>BURN 2015</h2>
+		<h1>5K &amp; HALF MARATHON</h1>
+		<p><strong>On Saturday, October 10, 2015</strong>, experience a festive walk or run through the
+			beautiful Coyote Hills Regional Park. Both 5K and Half Marathon courses are
+			supported by our dedicated BURN volunteers. You will never run out of
+			hydration, snacks and cheers! Half marathon finishers will receive an inaugural
+			BURN 5K &amp; Half Marathon commemorative run shirt. Bring your family members, friends, co-workers and community groups to this non-competitive, high energy, fun run and walk. <br>
+			<strong>Run with us, BURN bright, you will be our star!</strong>
+		</p>
 
-			<p>BURN is a fast growing non-profit running club rooted in the SF Bay Area promotes better health and better life. 
-			</p>
-		</div>
+		<p>BURN is a fast growing non-profit running club rooted in the SF Bay Area promotes better health and better life. 
+		</p>
 	</div>
 	<div class="time-local container">
 		<div class="row">
@@ -31,32 +29,33 @@
 			</div>
 		</div>
 	</div>
-	<div>
-		<div class="fg-text row">
-			<div class="details container text-left">
+	<div class="details container-fluid row">
+		<div class="fg-text">
+			<div class="details container text-left text-small">
 				<div class="directions col-xs-12 col-sm-4">
-					<h3>DIRECTIONS</h3>
-					<p>We provide free parking about 2.3 miles away from the start line at DeVry University, 6600 Dumbarton Circle, Fremont, CA 94555. </p>
-					<p>Coyote hills regional park is located at 8000 Patterson Ranch Rd, Fremont, CA 94555.</p>
+					<h4 class="text-center">PARKING</h4>
+					<p> Free parking is available at DeVry University </p>
+					<address>
+						<a target="_blank" href="https://goo.gl/aONMb5">6600 Dumbarton Cir., Fremont, CA 94555</a> 
+					</address>
+					<p>Please note that due to our arrangement with the park district, parking is not permitted inside Coyote Hill Regional Park and the dirt lot on Paseo Padre Parkway on race day. Drop-offs can be made on Commerce Dr. </p>
 				</div>
-
 				<div class="bib-pickup col-xs-12 col-sm-4">
-					<h3>BIB PICKUP</h3>
-					<p>Early bib pickup will be held on Friday Oct 9th, 12:00PM - 5:00PM at
-						Sports Basement 1177 Kern Ave., Sunnyvale, CA 94085 
-					</p>
-					<p>Race morning pickup: Saturday Oct 10th, 6:45AM - 7:30AM.
-					</p>
+					<h4 class="text-center">SHUTTLE</h4>
+					<p>Shuttle service will be provided from 7:00 am at DeVry University parking lot every 15 minutes. Priority goes to Half Marathon runners from 7:00 to 7:30 am, and to 5K runners from 7:30 to 8:00 am. Last shuttle from Finish Line to DeVry University is 11:30am. </p> 
 				</div>
-
-				<div class="gear-checkin col-xs-12 col-sm-4">
-					<h3>GEAR CHECKIN</h3>
-					<p>You can check-in one gear bag near the start line, and pick up by 12:30 PM
-						at the same place.
+				<div class="bib-pickup col-xs-12 col-sm-4">
+					<h4 class="text-center">BIB &amp; GEAR CHECKIN</h4>
+					<p>Early bib pickup is held on Friday Oct 9th, 12:00 - 5:00 pm at 
+						<address>
+							<a href="https://goo.gl/ERJyZ9" target="_blank">Sports Basement 1177 Kern Ave., Sunnyvale, CA 94085</a>. 
+						</address>
 					</p>
+					<p> Race morning pickup: Saturday Oct 10th, 6:45 - 7:30am near start line. </p>
+					<p>You can check-in one gear bag near the start line, and pick up by 12:30pm at the same place.</p>
 				</div>
 			</div>
-	</div>
-	<img class="center-crop" src="images/info-01.jpg" alt="info"/>
+		</div>
+		<img class="center-crop" src="images/info-01.jpg" alt="info"/>	</section>
 	</div>
 </section>
--- includes/04-course.php
+++ includes/04-course.php
@@ -1,6 +1,12 @@
 <section id="course" class="container-fluid text-center">
 	<h2>COURSE</h2>
 	<div class="maps container">
+		<div class="description col-xs-12 text-left text-small">
+			<p><strong>5K</strong> -  A single lap around Coyote Hills via Bayview Trail and Chochenyo Trail, mostly flat.</p>
+			<p><strong>Half Marathon</strong> - Two laps around Coyote Hills via Bayview Trail, each with an out-and-back addition via Apay Way to the 84 bridge overpass. 2nd lap turns off Bayview Trail on Quail Trail, with an additional loop around Meadowlark Trail at the end (with considerable elevation).</p>
+			<p><strong>Course</strong> - The trails are mixed surfaces, partially paved with some gravel and dirt trails, trail shoes are recommended. We will be sharing the park with local residents, horses, and dogs. Runners are advised to run in single file, keep to the right and share the road with cyclist and other users of the park. Course turn markers are color coded for 5K, Half Marathon 1st lap, and Half Marathon 2nd lap respectively. (Images to be added).</p>
+			<p><strong>Aid Stations</strong> - There are three aid stations along the course, 5K runners will find them at 1.2mi and 2.9mi, for Half Marathon runners, these two plus 4.2mi, 5.6mi, 7.0mi, 8.6mi, 10.0mi and 11.5mi. If you may require extra water in between, please bring your own water bottle.</p>
+		</div>
 		<div class="col-xs-12 col-sm-6">
 			<h2>13 miles</h2>
 			<h4>Half Marathon</h4>
