f17deef88183e738afb4b1c757cb7184fb599a40
chentingdong <chentingdong@gmail.com>
2015-10-05 13:15:46 -0700

    add logos

--- includes/09-sponsors.php
+++ includes/09-sponsors.php
@@ -20,7 +20,8 @@
 			<img class="col-xs-4 col-sm-3 col-md-2" src="images/sponsors/chinastix_logo.png" alt="China Stix" title="China Stix"/>
 			<img class="col-xs-4 col-sm-3 col-md-2" src="images/sponsors/eastbayregionalpark_logo.png" alt="East Bay Regional Park" title="East Bay Regional Park"/>
 			<img class="col-xs-4 col-sm-3 col-md-2" src="images/sponsors/wqzwdt_logo.png" alt="Bay Area Chinese Radio" title="Bay Area Chinese Radio"/>
-			<img class="col-xs-4 col-sm-3 col-md-2" src="images/sponsors/marina_logo.png" alt="Bay Area Chinese Radio" title="Bay Area Chinese Radio"/>
+			<img class="col-xs-4 col-sm-3 col-md-2" src="images/sponsors/marina_logo.png" alt="Marina Sea Food" title="Marina Sea Food"/>
+			<img class="col-xs-4 col-sm-3 col-md-2" src="images/sponsors/enspire_logo.png" alt="Enspire" title="Enspire"/>
 		</div>
 	</div>
 </section>
